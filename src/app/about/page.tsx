377392717d90c8e66799521398735ec5269eed5c
Parijat <parijatdas823@gmail.com>
2024-01-24 23:37:36 +0530

    AboutPage

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,8 @@
+import Aboutpage from "@/components/About/About";
 export default function About() {
   return (
     <div className="">
-      <h1 className="text-4xl font-bold">About</h1>
-      <p className="text-xl">This is the about page</p>
+      <Aboutpage/>
     </div>
   );
 }
